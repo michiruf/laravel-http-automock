9ade5667fe7109a8938e4b550516a3abc8c377ea
Michael Ruf <git@michiruf.de>
2025-02-19 15:35:49 +0100

    Code style

--- src/Resolver/Helper/FileNameSanitizer.php
+++ src/Resolver/Helper/FileNameSanitizer.php
@@ -9,6 +9,7 @@ class FileNameSanitizer
     public static function sanitize(string|Stringable $value, string $with = '-'): string|Stringable
     {
         static $replace = [':', '?', '*', '"', '<', '>', '|', '\r', '\n', '\t'];
+
         return $value instanceof Stringable
             ? $value->replace($replace, $with)
             : str($value)->replace($replace, $with)->value();
