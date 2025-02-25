0e0263ef53f329c8c8e8d9d69e05b78da4026920
Michael Ruf <git@michiruf.de>
2025-02-25 11:15:44 +0100

    Remove unnecessary null-safe operator

--- src/Resolver/RequestUrlResolver.php
+++ src/Resolver/RequestUrlResolver.php
@@ -77,7 +77,7 @@ class RequestUrlResolver implements FileNameResolverInterface
 
         $url = str(Uri::composeComponents(
             $this->scheme ? $uri->getScheme() : null,
-            $authority?->value() ?: null,
+            $authority->value() ?: null,
             $this->path ? $uri->getPath() : null,
             $this->query ? $uri->getQuery() : null,
             $this->fragment ? $uri->getFragment() : null,
